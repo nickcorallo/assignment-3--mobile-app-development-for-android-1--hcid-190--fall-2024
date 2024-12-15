52fffefa4858fd8e237a59a7d51868189b86d963
nickcorallo <corallo@hartford.edu>
2024-12-15 14:47:55 -0500

    add changes

--- app/src/main/java/com/example/diamondprogram/MainActivity.kt
+++ app/src/main/java/com/example/diamondprogram/MainActivity.kt
@@ -4,13 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.diamondprogram.ui.theme.DiamondprogramTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             DiamondprogramTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    DiamondApp(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +29,87 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun DiamondApp(modifier: Modifier = Modifier) {
+    var input by remember { mutableStateOf(TextFieldValue("")) }
+    var diamondOutput by remember { mutableStateOf("") }
+
+    Column(
         modifier = modifier
-    )
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        TextField(
+            value = input,
+            onValueChange = { input = it },
+            label = { Text("Enter a positive number") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = {
+            val number = input.text.toIntOrNull()
+            diamondOutput = if (number != null && number > 0) {
+                generateDiamond(number)
+            } else {
+                "Please enter a valid positive number."
+            }
+        }) {
+            Text("Generate Diamond")
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = diamondOutput, modifier = Modifier.fillMaxWidth())
+    }
+}
+
+fun generateDiamond(number: Int): String {
+    val stringBuilder = StringBuilder()
+
+    if (number % 2 == 0) {
+        for (i in 0 until number / 2) {
+            val spaceCount = number / 2 - i
+            val starCount = 2 * i + 1
+
+            stringBuilder.append(" ".repeat(spaceCount))
+            stringBuilder.append("*".repeat(starCount))
+            stringBuilder.append("\n")
+        }
+
+        for (i in number / 2 - 1 downTo 0) {
+            val spaceCount = number / 2 - i
+            val starCount = 2 * i + 1
+
+            stringBuilder.append(" ".repeat(spaceCount))
+            stringBuilder.append("*".repeat(starCount))
+            stringBuilder.append("\n")
+        }
+    } else {
+        for (i in 0 until number / 2 + 1) {
+            val spaceCount = number / 2 - i
+            val starCount = 2 * i + 1
+
+            stringBuilder.append(" ".repeat(spaceCount))
+            stringBuilder.append("*".repeat(starCount))
+            stringBuilder.append("\n")
+        }
+
+        for (i in 0 until number / 2) {
+            val spaceCount = i + 1
+            val starCount = number - 2 * (i + 1)
+
+            stringBuilder.append(" ".repeat(spaceCount))
+            stringBuilder.append("*".repeat(starCount))
+            stringBuilder.append("\n")
+        }
+    }
+
+    return stringBuilder.toString()
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun DiamondAppPreview() {
     DiamondprogramTheme {
-        Greeting("Android")
+        DiamondApp()
     }
 }
